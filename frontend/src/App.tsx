ac976ffe24822b94d7f8458338892bed36283c69
Jindrich Krupka <kruj08@vse.cz>
2025-06-30 19:08:19 +0200

    feat: Router setup for view-wishlist page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import WishlistHub from './features/WishlistHub/WishlistHub.tsx';
 import MyIdeas from './features/MyIdeas/MyIdeas.tsx';
 import Calendar from './features/Calendar/Calendar.tsx';
 import LandingPage from './features/Landing/LandingPage.tsx';
+import ViewWishlist from './features/viewWishlist/viewWishlist.tsx';
 
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
@@ -23,6 +24,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
+            <Route path="view-wishlist" element={<ViewWishlist />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </GoogleOAuthProvider>
@@ -40,6 +42,7 @@ const App = () => {
           <Route path="hub" element={<WishlistHub />} />
           <Route path="ideas" element={<MyIdeas/>} />
           <Route path="calendar" element={<Calendar/>} />
+          <Route path="view-wishlist" element={<ViewWishlist />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
 
---
+++ frontend/src/features/viewWishlist/viewWishlist.tsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+
+const ViewWishlist = () => {
+    return (
+        <div>
+            <h1>Under construction</h1>
+            <p>This feature is currently under construction. Please check back later!</p>
+        </div>
+    )
+}
+
+export default ViewWishlist;
